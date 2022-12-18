eab86a15e9a2ae6b70011dc1d93f933531754b43
xz <xz@xzdeMacBook-Air.local>
2022-12-17 21:39:20 -0500

    front end update

--- Codes/JavaProject/src/main/resources/static/app.js
+++ Codes/JavaProject/src/main/resources/static/app.js
@@ -11,41 +11,38 @@ function setConnected(connected) {
     }
     $("#greetings").html("");
 }
-
 function connect() {
     var socket = new SockJS('/java_project');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/game_room', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
-        });
+        stompClient.subscribe('/topic/game_room', func);
     });
-}
-
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
 }
 
 function sendName() {
     stompClient.send("/app/room/join", {}, JSON.stringify({'userId': $("#name").val(), 'roomId': 1}));
 }
-
+function func(greeting) {
+    receivedObject = JSON.parse(greeting.body);
+    console.log(receivedObject['type']);
+    showGreeting("Hello user " + JSON.stringify(receivedObject['ownerId']) + ", ready in owner: " + JSON.stringify(receivedObject['ownerId']) + "'s room " + JSON.stringify(receivedObject['roomId']));
+}
 function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
-
+function start() {
+    stompClient.send("/app/room/start", {}, JSON.stringify({'userId': $("#name").val(), 'roomId': 1}));
+}
+function gameStart() {
+    window.location.replace("./game.html");
+}
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
+    connect();
     $( "#send" ).click(function() { sendName(); });
 });
 
